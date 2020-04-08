ce0271c845a584d92d139585e29379858c2abb31
ivan <ivanrg99@tutanota.com>
2020-04-08 18:51:07 +0200

    added pixijs

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,11 +3,15 @@ import {
   AfterViewInit,
   ViewChild,
   ElementRef,
+  OnInit,
 } from '@angular/core';
 import { SelectorService } from '../selector.service';
 import { Item } from '../item';
 import { BoardService } from '../board.service';
 import { Drawable } from '../drawable';
+
+import * as PIXI from 'pixi.js';
+import { getTestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-board',
@@ -15,42 +19,16 @@ import { Drawable } from '../drawable';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements AfterViewInit {
-  @ViewChild('canvas') canvas: ElementRef;
-  height = 600;
-  width = 795;
-  private context: CanvasRenderingContext2D;
-  items: Item[] = [];
-
-  constructor(private selectorService: SelectorService, private boardService: BoardService) {
-  }
-
+  @ViewChild('boardpx') boardpx: ElementRef;
   ngAfterViewInit(): void {
-    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
-    this.context = canvasEl.getContext('2d', {
-      alpha: false
-    });
-
-    canvasEl.width = this.width;
-    canvasEl.height = this.height;
-
-    this.context.lineWidth = 2;
-    this.context.lineCap = 'round';
-    this.context.strokeStyle = '#000';
-
-    canvasEl.addEventListener('mousedown', this.captureEvents.bind(this));
-
-    this.boardService.setDrawable(new Drawable(this.context, canvasEl));
-    this.boardService.flush();
-
-  }
-
-  //TODO: Make board service that talks with renderer
-  private captureEvents(ev: MouseEvent) {
-    const x = ev.clientX;
-    const y = ev.clientY;
-    const type = this.selectorService.getSelectedItem();
-    if (type == undefined) return;
-    this.boardService.addItem(new Item(x, y, type, "#d3d3d3", type));
+    const boardEl: HTMLElement = this.boardpx.nativeElement;
+    const pixi = new PIXI.Application( { antialias: true});
+    boardEl.appendChild(pixi.view);
+    const gr = new PIXI.Graphics();
+    gr.beginFill(0xDE3249);
+    gr.drawRect(50, 50, 100, 100);
+    gr.endFill();
+    pixi.stage.addChild(gr);
   }
 
 }
